120ae6779c9e71e288d83e1b4ba61f0e57de7257
Ferdinand Bhavsar <ferdinand.bhavsar@epita.fr>
2019-02-28 19:35:02 +0100

    commit

--- main.cc
+++ main.cc
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <sstream>
 #include <unordered_set>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -19,6 +21,38 @@ public:
     void parse(int argc, char* argv[])
     {
         std::cout << argv[1] << '\n';
+        std::fstream fs;
+        fs.open(argv[1]);
+
+        std::string str;
+        getline(fs, str);
+        size_t nb = atol(str.c_str());
+        auto v = std::vector<Slide>();
+        for (auto i = 0u; i < nb; ++i)
+        {
+            auto S = Slide();
+            size_t pos = 0;
+            getline(fs, str);
+            auto new_pos = str.find(" ", pos);
+            auto tok = str.substr(pos, new_pos);
+            S.horizontal = tok[0] == 'H';
+            S.id = i;
+            pos = new_pos;
+            new_pos = str.find(" ", pos + 1);
+            tok = str.substr(pos, new_pos);
+            auto tags = atol(tok.c_str());
+            auto end = str.find("\n");
+            for (auto i = 0u; i < tags - 1; ++i)
+            {
+                pos = new_pos;
+                new_pos = str.find(" ", pos + 1);
+                tok = str.substr(pos, new_pos);
+                S.tags.emplace(tok);
+            }
+            tok = str.substr(pos, end);
+            S.tags.emplace(tok);
+        }
+        fs.close();
     }
 
     void execute()
